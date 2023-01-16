6d7d15adfed0591818750f61db7849033a79cc7f
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-01-16 10:16:21 +0200

    Cleanup for old package

--- multiversx_sdk_core/transaction_builders/relayed_builders.py
+++
@@ -1,8 +0,0 @@
-class RelayedTransactionV1Builder:
-    def __init__(self) -> None:
-        raise NotImplementedError()
-
-
-class RelayedTransactionV2Builder:
-    def __init__(self) -> None:
-        raise NotImplementedError()

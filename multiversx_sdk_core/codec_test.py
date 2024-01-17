cdcaeeeb625403b75950a827b0076b891d20ec0c
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2024-01-17 14:11:46 +0200

    Extra tests.

--- multiversx_sdk_core/codec_test.py
+++ multiversx_sdk_core/codec_test.py
@@ -6,3 +6,5 @@ def test_encode_signed_number():
     assert codec.encode_signed_number(-0x11) == bytes([0xEF])
     assert codec.encode_signed_number(-128) == bytes([0x80])
     assert codec.encode_signed_number(-1) == bytes([0xFF])
+    assert codec.encode_signed_number(0) == bytes([])
+    assert codec.encode_signed_number(1) == bytes([0x01])
